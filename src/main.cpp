48573865a5a8779e383f4aedbcb82e027aeec8dd
Andrey Denisov <and.denisov26@gmail.com>
2025-10-31 21:10:09 +0300

    Fix main

--- src/main.cpp
+++ src/main.cpp
@@ -6,43 +6,40 @@
 using namespace std;
 
 int main() {
-    cout << "=== Демонстрация работы с фигурами ===" << endl;
-    
     Array<shared_ptr<Figure<int>>> figures;
     
     // Создание ромба
     auto rhombus = make_shared<Rhombus<int>>();
-    cout << "Введите 4 точки для ромба (x y):" << endl;
+    cout << "Введите 4 точки для ромба (x y):" << "\n";
     cin >> *rhombus;
     figures.push_back(rhombus);
     
     // Создание трапеции
     auto trapezoid = make_shared<Trapezoid<int>>();
-    cout << "Введите 4 точки для трапеции (x y):" << endl;
+    cout << "Введите 4 точки для трапеции (x y):" << "\n";
     cin >> *trapezoid;
     figures.push_back(trapezoid);
     
     // Создание пятиугольника
     auto pentagon = make_shared<Pentagon<int>>();
-    cout << "Введите 5 точек для пятиугольника (x y):" << endl;
+    cout << "Введите 5 точек для пятиугольника (x y):" << "\n";
     cin >> *pentagon;
     figures.push_back(pentagon);
     
     // Вывод информации о фигурах
-    cout << "\nИнформация о фигурах" << endl;
+    cout << "\nИнформация о фигурах" << "\n";
     double total_area = 0;
     for (size_t i = 0; i < figures.size(); ++i) {
-        cout << "Фигура " << i << ":" << endl;
+        cout << "Фигура " << i << ":" << "\n";
         cout << *figures[i];
         double area = static_cast<double>(*figures[i]);
-        cout << "Площадь: " << area << endl;
+        cout << "Площадь: " << area << "\n";
         auto center = figures[i]->center();
-        cout << "Центр: (" << center.getX() << ", " << center.getY() << ")" << endl;
+        cout << "Центр: (" << center.getX() << ", " << center.getY() << ")" << "\n";
         total_area += area;
-        cout << "------------------------" << "\n";
     }
     
-    cout << "Общая площадь всех фигур: " << total_area << endl;
+    cout << "Общая площадь всех фигур: " << total_area << "\n";
     
     return 0;
 }
